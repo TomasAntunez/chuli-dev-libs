1a8dee1a40846cc5d6508fefdf5a22827b45cda6
Chuli <tomasantunez.dev@gmail.com>
2026-01-30 21:10:43 -0300

    test: change node ts config

--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -1,1 +1,12 @@
 console.log('Hello, world!');
+
+export const getSomething = (bool?: boolean) => {
+  if (bool) {
+    return;
+  }
+
+  return 'You passed nothing!';
+};
+
+const result = getSomething();
+console.log(result);
